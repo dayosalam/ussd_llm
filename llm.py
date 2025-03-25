842d40f8f5e010410d8df28022933a41ddc1aeb9
Dayo Salam <dayosalamboi@gmail.com>
2025-03-25 17:14:30 +0100

    added the env in the code

--- llm.py
+++ llm.py
@@ -2,12 +2,12 @@
 import getpass 
 import os 
 
-def _set_env(var: str):
-    if not os.environ.get(var):
-        os.environ[var] = getpass.getpass(f"{var}: ")
+# def _set_env(var: str):
+#     if not os.environ.get(var):
+#         os.environ[var] = getpass.getpass(f"{var}: ")
 
 
-_set_env("GROQ_API_KEY")
+# _set_env("GROQ_API_KEY")
 
 
 #%%
