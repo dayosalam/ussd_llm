facb4fe48c9038e660fd1b6816211b941b143c6c
Dayo Salam <dayosalamboi@gmail.com>
2025-03-25 19:17:34 +0100

    editted something

--- llm.py
+++ llm.py
@@ -61,23 +61,10 @@ graph = graph_builder.compile()
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
         for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+            print(value["messages"][-1].content)
 
 # Ensure the script only runs when executed directly, not when imported
-if __name__ == "__main__":
-    while True:
-        try:
-            user_input = input("User: ")
-            if user_input.lower() in ["quit", "exit", "q"]:
-                print("Goodbye!")
-                break
+# if __name__ == "__main__":
 
-            stream_graph_updates(user_input)
-        except:
-            # Fallback if input() is not available
-            user_input = "What do you know about LangGraph?"
-            print("User: " + user_input)
-            stream_graph_updates(user_input)
-            break
 
 #%%
