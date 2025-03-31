67e399912bb0e45032f7cb6b41063026eee82354
Dayo Salam <dayosalamboi@gmail.com>
2025-03-31 19:34:08 +0100

    Sms working, implemented the database using sqlite3 and number for id

--- llm.py
+++ llm.py
@@ -1,39 +1,96 @@
 #%%
 import getpass 
 import os 
-
-# def _set_env(var: str):
-#     if not os.environ.get(var):
-#         os.environ[var] = getpass.getpass(f"{var}: ")
-
-
-# _set_env("GROQ_API_KEY")
-
-
-#%%
 from typing import Annotated
-
 from typing_extensions import TypedDict
-
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langchain_community.tools.tavily_search import TavilySearchResults
+import json
+from langchain_core.messages import ToolMessage
+# from langgraph.checkpoint.memory import MemorySaver
+from langchain_groq import ChatGroq
+from IPython.display import Image, display
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
+#%%
+sqlite_conn = sqlite3.connect("ussd.sqlite3", check_same_thread=False)
 
+memory =SqliteSaver(sqlite_conn)
 
+#%%
+# memory = MemorySaver()
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
+    
+tool = TavilySearchResults(max_results=2, tavily_api_key= "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")
+tools = [tool]
+# tool.invoke("What's a 'node' in LangGraph?")
+#%%
+#ToolNode
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+  
+ 
+#tools_condition
+def route_tools(
+    state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+
 
 
 graph_builder = StateGraph(State)
 #%%
-from langchain_groq import ChatGroq
+
 
 GROQ_API_KEY = "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"
 # Initialize LLM
-llm = ChatGroq(temperature=0, model="llama-3.1-8b-instant", groq_api_key=GROQ_API_KEY)
+llm = ChatGroq(temperature=0, model="llama-3.3-70b-specdec", groq_api_key=GROQ_API_KEY)
+llm_with_tools = llm.bind_tools(tools)
+
+
+tool_node = BasicToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node) 
 
 def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
@@ -43,14 +100,22 @@ def chatbot(state: State):
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
-#%%
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+    # It defaults to the identity function, but if you
+    # want to use a node named something else apart from "tools",
+    # You can update the value of the dictionary to something else
+    # e.g., "tools": "my_tools"
+    {"tools": "tools", END: END},
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
+graph = graph_builder.compile(checkpointer=memory)
 
-graph_builder.add_edge("chatbot", END)
-
-graph = graph_builder.compile()
-
-# from IPython.display import Image, display
 
 # try:
 #     display(Image(graph.get_graph().draw_mermaid_png()))
@@ -58,16 +123,31 @@ graph = graph_builder.compile()
 #     # This requires some extra dependencies and is optional
 #     pass
 #%%
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-        for value in event.values():
-            response_text += value["messages"][-1].content + "\n"  # Append messages
 
-            
-    return response_text.strip() if response_text else "END Sorry, no response available."
-
-# Ensure the script only runs when executed directly, not when imported
-# if __name__ == "__main__":
+def stream_graph_updates(user_input: str,  phone_number: str):
+    config = {"configurable": {"thread_id": phone_number}}
+    response = graph.invoke({"messages": [{"role": "user", "content": user_input}]},
+                            config)
+    text = response["messages"][-1].content
+    return text
 
 
 #%%
+# while True:
+#     try:
+#         user_input = input("User: ")
+#         if user_input.lower() in ["quit", "exit", "q"]:
+#             print("Goodbye!")
+#             break
+
+#         print(stream_graph_updates(user_input))
+#     except:
+#         # fallback if input() is not available
+#         user_input = "What do you know about LangGraph?"
+#         print("User: " + user_input)
+#         stream_graph_updates(user_input)
+#         break
+
+# %%
+
+# %%
--- ussd.py
+++ ussd.py
@@ -13,6 +13,7 @@ from functools import partial
 from contextlib import redirect_stdout
 import io
 
+SANDBOX_API_KEY = "atsk_b1b293e92ed2fc4b5b843ea5a4ed6fabfbb7c0c6605c6047efe9799878b38e3c36e72479"
 app = Flask(__name__)
 
 response = ""
@@ -21,71 +22,71 @@ response = ""
 
 # Replace with your OpenAI or local LLM API key
 
-GROQ_API_KEY = "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"
-# Initialize LLM
-llm_groq = ChatGroq(temperature=0, model="llama-3.1-8b-instant", groq_api_key=GROQ_API_KEY)
+# GROQ_API_KEY = "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"
+# # Initialize LLM
+# llm_groq = ChatGroq(temperature=0, model="llama-3.1-8b-instant", groq_api_key=GROQ_API_KEY)
 
-def query_llm():
-    """Builds the LangChain pipeline."""
-    template = """Based on the context below, write a simple response that would answer the user's question. 
-    Use the following pieces of retrieved-context to answer the question. 
-    If you don't know the answer, say that you don't know.
-    Use three sentences maximum and keep the answer concise.
-    In your response, go straight to answering.
+# def query_llm():
+#     """Builds the LangChain pipeline."""
+#     template = """Based on the context below, write a simple response that would answer the user's question. 
+#     Use the following pieces of retrieved-context to answer the question. 
+#     If you don't know the answer, say that you don't know.
+#     Use three sentences maximum and keep the answer concise.
+#     In your response, go straight to answering.
 
-    Question: {question}
-    """
+#     Question: {question}
+#     """
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", """
-                You are a Super Intelligent chatbot with Advanced Capabilities. 
-                You are a chatbot that can answer any question with a superhero joke.
-                You are limited to 150 characters of response.
-            """),
-            MessagesPlaceholder(variable_name="history"),
-            ("human", template),
-        ]
-    )
+#     prompt = ChatPromptTemplate.from_messages(
+#         [
+#             ("system", """
+#                 You are a Super Intelligent chatbot with Advanced Capabilities. 
+#                 You are a chatbot that can answer any question with a superhero joke.
+#                 You are limited to 150 characters of response.
+#             """),
+#             MessagesPlaceholder(variable_name="history"),
+#             ("human", template),
+#         ]
+#     )
 
-    memory = ConversationBufferMemory(return_messages=True)
+#     memory = ConversationBufferMemory(return_messages=True)
 
-    # LangChain pipeline
-    rag_chain = (
-        RunnablePassthrough.assign(
-            history=RunnableLambda(lambda x: memory.load_memory_variables(x)["history"])
-        )
-        | prompt
-        | llm_groq
-        | StrOutputParser()
-    )
+#     # LangChain pipeline
+#     rag_chain = (
+#         RunnablePassthrough.assign(
+#             history=RunnableLambda(lambda x: memory.load_memory_variables(x)["history"])
+#         )
+#         | prompt
+#         | llm_groq
+#         | StrOutputParser()
+#     )
 
-    return rag_chain
+#     return rag_chain
 
-def capture_output(user_input):
-    # Capture the printed output from stream_graph_updates
-    output = io.StringIO()
-    with redirect_stdout(output):
-        stream_graph_updates(user_input)
-    return output.getvalue().strip().replace('Assistant: ', '')
+# def capture_output(user_input):
+#     # Capture the printed output from stream_graph_updates
+#     output = io.StringIO()
+#     with redirect_stdout(output):
+#         stream_graph_updates(user_input)
+#     return output.getvalue().strip().replace('Assistant: ', '')
 
-@app.route('/')
-def home():
-    return render_template('ok.html')
+# @app.route('/')
+# def home():
+#     return render_template('ok.html')
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.json
-    user_message = data.get('message', '')
+# @app.route('/chat', methods=['POST'])
+# def chat():
+#     data = request.json
+#     user_message = data.get('message', '')
     
-    if not user_message:
-        return jsonify({'error': 'No message provided'}), 400
+#     if not user_message:
+#         return jsonify({'error': 'No message provided'}), 400
 
-    try:
-        response = capture_output(user_message)
-        return jsonify({'response': response})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+#     try:
+#         response = capture_output(user_message)
+#         return jsonify({'response': response})
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
     
 @app.route('/', methods=['POST', 'GET'])
 def ussd_callback():
@@ -110,6 +111,49 @@ def ussd_callback():
 
     print(f"Sending Response: {response}")  # Debugging
     return response  # Ensure a valid response is always returned
+
+
+@app.route('/sms_callback', methods=['POST'])
+def sms_callback():
+    print(request.method)
+    print(request.form)
+    print(request.form["from"])
+    text = request.form["text"]
+    phone_number = request.form["from"]
+    print(text)
+    if text.lower() in ["quit", "exit", "q"]:
+            messages = "END Goodbye! Thanks for using AI Chatbot."
+    else:
+        try:
+            ai_response = stream_graph_updates(text, phone_number)
+            messages = f"{ai_response}" if ai_response else "END Sorry, no response available."
+        except Exception as e:
+            print(f"Error: {e}")  # Debugging
+            messages = "END Sorry, something went wrong."
+    
+    response_sms(request.form["from"], messages)
+    return "Success", 201
+
+def response_sms(recipient_phone_number, message):
+    requests.post(
+        "https://api.sandbox.africastalking.com/version1/messaging",
+        data = {
+            "username": "sandbox",
+            "to": recipient_phone_number,
+            "message": message,
+            "from": "47532"
+            
+        },
+        headers = {
+            "apiKey": SANDBOX_API_KEY,
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+            
+}
+    )
+    
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)  # `host="0.0.0.0"` allows external access
